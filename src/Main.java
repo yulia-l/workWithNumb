900ddcdf2c900015ca07a6a37262ad5f6df20ca0
Люгуная Юлия <yulia.lyugunaya@yandex.ru>
2022-09-06 17:22:57 +0300

    v.1

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        List<Integer> positiv = new ArrayList<>();
+
+        for (int i = 0; i < intList.size(); i++) {
+            if (intList.get(i) > 0 && intList.get(i) % 2 == 0) {
+                positiv.add(intList.get(i));
+            }
+        }
+
+        positiv.sort(Comparator.naturalOrder());
+        System.out.println(positiv);
+    }
+}
